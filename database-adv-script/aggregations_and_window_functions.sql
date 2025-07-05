a9fb6e8566dd161d18ced765e271069512fce09a
Immanuel N <immanuelnjogu@students.tukenya.ac.ke>
2025-07-06 01:35:40 +0300

    aggregation

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -22,3 +22,13 @@ FROM
     bookings
 GROUP BY
     property_id;
+
+-- Query 3: Rank properties based on the total number of bookings using ROW_NUMBER()
+SELECT
+    property_id,
+    COUNT(*) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS booking_row_number
+FROM
+    bookings
+GROUP BY
+    property_id;
